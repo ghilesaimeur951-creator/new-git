354a9f117ace1b7422796bf33b76e51f58b9e5db
ghilesaimeur951-creator <ghilesaimeur951@gmail.com>
2026-09-25 16:11:22 +0200

    Ajoute JGit et le desugaring pour le mode GitHub réel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,5 +17,24 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
+
+    packaging {
+        resources {
+            excludes += setOf(
+                "META-INF/DEPENDENCIES",
+                "META-INF/LICENSE",
+                "META-INF/LICENSE.txt",
+                "META-INF/NOTICE",
+                "META-INF/NOTICE.txt",
+                "META-INF/versions/9/OSGI-INF/MANIFEST.MF"
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation("org.eclipse.jgit:org.eclipse.jgit:6.10.1.202505221210-r")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 }
