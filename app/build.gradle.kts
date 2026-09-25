f125f9aaebf8abf16ff2e8b734c2077bc79792da
ghilesaimeur951-creator <ghilesaimeur951@gmail.com>
2026-09-25 15:28:43 +0200

    Passe Ubuntu Git Academy en version 2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.ghiles.quizubuntu"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "2.0"
+        versionCode = 4
+        versionName = "2.1"
     }
 
     compileOptions {
