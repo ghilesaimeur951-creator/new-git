36f840199ed53d1de902aa2d456ef4e6c96c25a9
ghilesaimeur951-creator <ghilesaimeur951@gmail.com>
2026-09-26 03:37:57 +0200

    Garantit Ed25519 avec Bouncy Castle dans Ubuntu Lab

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
 dependencies {
     implementation("org.eclipse.jgit:org.eclipse.jgit:6.10.1.202505221210-r")
     implementation("org.eclipse.jgit:org.eclipse.jgit.ssh.apache:6.10.1.202505221210-r")
+    implementation("org.bouncycastle:bcprov-jdk18on:1.84")
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
     testImplementation("junit:junit:4.13.2")
 }
